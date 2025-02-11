1a46407d109a4c4d42d8c355a2fb107a618a3f6b
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-02-11 08:53:04 +0300

    Add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+  validates :name, :price, :quantity, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
