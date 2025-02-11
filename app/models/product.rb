19ad55ce84f2d477e96323345062edc3f9150465
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-02-11 08:57:58 +0300

    Broadcast stock updates via turbo stream

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  after_update_commit -> { broadcast_replace_to "products", partial: "products/product", locals: { product: self } }
+
   has_one_attached :image
   validates :name, :price, :quantity, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
